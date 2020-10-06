6a2875cc0234c04234512a6639315c3603417f26
unais ali <51183141+unaisshazan@users.noreply.github.com>
2020-10-07 00:03:45 +0500

    add

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <div>
+           <h1>I am components</h1> 
+           <img src="C:/Users/unais/Documents/GitHub/React-Ui-ux/src/components/assets/backgrounds/Group 1131.png" alt=""/>
+        </div>
+    )
+}
+
+export default Footer
