8bb37ba3dd04249b59e0ab4e2c59f0467e04edee
unais ali <51183141+unaisshazan@users.noreply.github.com>
2020-10-07 00:13:52 +0500

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import '../App.css'
+import Image from 'react-bootstrap/Image'
 function Footer() {
     return (
         <div>
            <h1>I am components</h1> 
-           <img src="https://i.ibb.co/rvW6C94/Group-1131.png" alt="bg"/>
+           <Image src="https://i.ibb.co/rvW6C94/Group-1131.png" alt="bg" fluid>
         </div>
     )
 }
